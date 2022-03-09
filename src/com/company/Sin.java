7113f7f600cc07b59d4fd8c2191e0d2c358637a8
FruteL <kpavel2000@gmail.com>
2022-03-09 15:47:46 +0200

    Cycle practice. The exercise 3 (Integral)

---
+++ src/com/company/Sin.java
@@ -0,0 +1,37 @@
+package com.company;
+
+/**
+ * CalcIntegral - function that calculate area under sinusoidal curve
+ * Param:
+ *      steps - responsible for the accuracy of the calculation
+ *      startX, endX - responsible for limiting the X axis (start and end point respectively)
+ *      BlockY - is responsible for limiting the Y-axis (from zero to the specified value).
+ *      The value 0 means that the limit is not set.
+ */
+public class Sin {
+    // todo - сделать норм функцию, что принимает параметры
+    public static void main(String[] args){
+        System.out.println(calcIntegralSin(10, 0, Math.PI, 0.9));
+    }
+
+    public static double calcIntegralSin (int steps, double startX, double endX, double blockY){
+        double width = (endX - startX)/ steps;
+        double area = 0;
+        for (int i = 0; i < steps - 1; i++) {
+            if (i >= startX) {
+                double hight = Math.sin(i * width);
+                if (hight >= blockY && blockY!=0)
+                    hight = blockY;
+                area += width * hight;
+            }
+        }
+        return area;
+    }
+
+
+
+
+
+
+
+}
